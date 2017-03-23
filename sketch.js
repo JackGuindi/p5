7a749dd2b66b0fbdec06912726e719884df641d0
Jack Guindi <jack.guindi@mdycehs.net>
2017-03-23 14:43:52 +0000

    olympic rings

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(900, 900);
+}
+
+
+function draw() {
+  stroke(0, 0, 0);
+	strokeWeight(10);
+	fill(255, 255, 255, .5);
+	ellipse(330, 150, 160, 160);
+	
+	stroke(65, 65, 244);
+	strokeWeight(10);
+	fill(255, 255, 255, .5);
+	ellipse(150, 150, 160, 160);
+	
+	stroke(244, 66, 66);
+	strokeWeight(10);
+	fill(255, 255, 255, .5);
+	ellipse(510, 150, 160, 160);
+	
+	stroke(241, 244, 65);
+	strokeWeight(10);
+	fill(255, 255, 255, .5);
+	ellipse(240, 250, 160, 160);
+	
+	stroke(82, 244, 65);
+	strokeWeight(10);
+	fill(255, 255, 255, .5);
+	ellipse(440, 250, 160, 160);
+	
+    
+}
